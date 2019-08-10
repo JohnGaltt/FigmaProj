33c5f1096ef88000d34af5549705a5450c08aac6
oleksandr.zholob <johngaltaregoing@gmail.com>
2019-08-10 10:37:32 +0300

    init commint

---
+++ function.js
@@ -0,0 +1,14 @@
+payment_expired()
+{
+    // hide QR code and show "Invoice Expired" with red background
+}
+
+payment_detected()
+{
+    //  hide qr code and show the squirell
+}
+
+payment_completed()
+{
+    // redirect to "/PaymentCompleted?ID_TRANSACTION=3" page
+}
